d386ffe1e98fd6ab524a7b8c320e06cca5ee2564
PushpenderRana <pushpenderrana270@gmail.com>
2026-07-20 22:39:20 +0530

    modify app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from routes.recommendation import recommendation_bp
 
 app = Flask(__name__)
@@ -15,5 +16,13 @@ def home():
     }
 
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
